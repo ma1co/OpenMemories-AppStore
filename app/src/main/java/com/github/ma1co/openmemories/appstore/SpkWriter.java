9dde49aaae43aaab215e4d3c1c27ec438f4d53f1
ma1co <ma1co@users.noreply.github.com>
2016-07-14 21:30:44 +0000

    Edit SpkWriter

--- app/src/main/java/com/github/ma1co/openmemories/appstore/SpkWriter.java
+++ app/src/main/java/com/github/ma1co/openmemories/appstore/SpkWriter.java
@@ -1,5 +1,7 @@
 package com.github.ma1co.openmemories.appstore;
 
+import android.util.Base64;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
@@ -11,28 +13,28 @@ public class SpkWriter extends OutputStream {
     private final int blockSize = 0x100000;
 
     // from TouchLessShutter100.1.spk
-    private final byte[] header = Util.fromHex("3173706b00000000000100007e29351425ec82c61ef1d736afadc280966a2dadd53ffee3d55e608afad43951853a1be9e36265b05c1e4345ac4919d6c9ef4e021f58bf85e4851c3cb2bd14416d261526296523259664be8ac2477f7bd6d1c162af286c65a7f57a1800e489cf24fc58fb041f29ea103f5fca3eb996baaf8eea2cfd6431bb765dced8110b341db6d313dc40a82a6e2134272e3aa3c7570b80d5d1d7533f2efcf3ff0a00f91691847417005da841fd2906ac7e0767fafbbb1e7ea0e1c76828ace66fdd449506609cc604c8abf11d9414a7cb296d93841b3a062c05e5a3f340556dc25e6c0e467466242ff433cc200ef9f29d9bbdf6227212ef40bc43cb74b1df02be3153a334a2");
-    private final byte[] key = Util.fromHex("c301afd22ddba4c090eca46258e4bfb1");
+    private final byte[] header = Base64.decode("MXNwawAAAAAAAQAAfik1FCXsgsYe8dc2r63CgJZqLa3VP/7j1V5givrUOVGFOhvp42JlsFweQ0WsSRnWye9OAh9Yv4XkhRw8sr0UQW0mFSYpZSMllmS+isJHf3vW0cFiryhsZaf1ehgA5InPJPxY+wQfKeoQP1/KPrmWuq+O6iz9ZDG7dl3O2BELNB220xPcQKgqbiE0Jy46o8dXC4DV0ddTPy788/8KAPkWkYR0FwBdqEH9KQasfgdn+vu7Hn6g4cdoKKzmb91ElQZgnMYEyKvxHZQUp8spbZOEGzoGLAXlo/NAVW3CXmwORnRmJC/0M8wgDvnynZu99iJyEu9AvEPLdLHfAr4xU6M0og==", Base64.DEFAULT);
+    private final byte[] key = Base64.decode("wwGv0i3bpMCQ7KRiWOS/sQ==", Base64.DEFAULT);
 
     private final OutputStream outputStream;
     private final Cipher cipher;
     private final byte[] buffer = new byte[blockSize];
     private int bufferOffset = 0;
+    private boolean headerWritten = false;
 
-    public SpkWriter(OutputStream outputStream) throws GeneralSecurityException, IOException {
+    public SpkWriter(OutputStream outputStream) throws GeneralSecurityException {
         this.outputStream = outputStream;
         cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"));
-        writeHeader();
     }
 
     @Override
-    public void write(final byte[] data, final int offset, final int length) throws IOException {
-        int written = 0;
-        while (written < length) {
-            int n = Math.min(length - written, buffer.length - bufferOffset);
-            System.arraycopy(data, offset + written, buffer, bufferOffset, n);
-            written += n;
+    public void write(byte[] data, int offset, int length) throws IOException {
+        length += offset;
+        while (offset < length) {
+            int n = Math.min(length - offset, buffer.length - bufferOffset);
+            System.arraycopy(data, offset, buffer, bufferOffset, n);
+            offset += n;
             bufferOffset += n;
             if (bufferOffset == buffer.length)
                 writeBuffer();
@@ -52,9 +54,12 @@ public class SpkWriter extends OutputStream {
 
     private void writeHeader() throws IOException {
         outputStream.write(header);
+        headerWritten = true;
     }
 
     private void writeBuffer() throws IOException {
+        if (!headerWritten)
+            writeHeader();
         if (bufferOffset > 0) {
             try {
                 outputStream.write(cipher.doFinal(buffer, 0, bufferOffset));
